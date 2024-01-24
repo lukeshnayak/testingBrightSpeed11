8af66c3882410686214eb8b65907e6b88b3bba32
LukeshKumarNayak <LukeshKumarNayak@Rev-PG035RLK>
2024-01-24 09:14:40 -0800

    CucumberRepo1

---
+++ MavenCucumber_A1/src/main/java/cucumberBDDone/MavenCucumber_A1/App.java
@@ -0,0 +1,13 @@
+package cucumberBDDone.MavenCucumber_A1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
